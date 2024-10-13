88c17311233099e9ff8308765b30e695d140d2f8
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-10-13 09:23:43 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace komponentova_tvorba.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
