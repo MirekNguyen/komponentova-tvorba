cd28186192ede6a26b4c04ec025afc4f82929c9a
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-12-19 20:41:06 +0100

    feat: migrate image url

---
+++ Migrations/20241219192726_AddImageUrlBookProperty.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace komponentova_tvorba.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageUrlBookProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ISBN",
+                table: "Books",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Books",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ISBN",
+                table: "Books",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
