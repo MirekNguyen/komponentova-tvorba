bbde3b3604254ae0a5a12b5bfc71e54f1b6f4402
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-12-20 01:27:43 +0100

    feat: message after due date

--- Models/LoanModel.cs
+++ Models/LoanModel.cs
@@ -1,30 +1,40 @@
-namespace komponentova_tvorba.Models;
-
-public class Loan
+namespace komponentova_tvorba.Models
 {
-    public int Id { get; set; }
-    public required Book Book { get; set; }
-    public DateOnly LoanDate { get; set; }
-    public DateOnly? ReturnDate { get; set; }
-    public DateOnly DueDate { get; set; }
-    public int Amount { get; set; }
-    public required Reader Reader { get; set; }
-
-    public int DaysLeft
+    public class Loan
     {
-        get
+        public int Id { get; set; }
+        public required Book Book { get; set; }
+        public DateOnly LoanDate { get; set; }
+        public DateOnly? ReturnDate { get; set; }
+        public DateOnly DueDate { get; set; }
+        public int Amount { get; set; }
+        public required Reader Reader { get; set; }
+
+        public int DaysLeft
         {
-            var dueDateTime = DueDate.ToDateTime(new TimeOnly(0, 0));
-            return (dueDateTime - DateTime.Today).Days;
+            get
+            {
+                var dueDateTime = DueDate.ToDateTime(new TimeOnly(0, 0));
+                return (dueDateTime - DateTime.Today).Days;
+            }
         }
-    }
 
-    public bool IsReturned => ReturnDate.HasValue;
-    public bool IsLateReturn
-    {
-        get
+        public bool IsReturned => ReturnDate.HasValue;
+
+        public bool IsLateReturn
         {
-            return IsReturned && ReturnDate.HasValue && ReturnDate.Value > DueDate;
+            get
+            {
+                return IsReturned && ReturnDate.HasValue && ReturnDate.Value > DueDate;
+            }
+        }
+
+        public bool IsOverdue
+        {
+            get
+            {
+                return !IsReturned && DateTime.Today > DueDate.ToDateTime(TimeOnly.MinValue);
+            }
         }
     }
 }
